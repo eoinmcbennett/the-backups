8b588ef2a88491f4983059f5674b02146141be3b
Taylor Crawford <tcrawford540@gmail.com>
2023-10-12 19:44:52 +0100

    [003] Add Insert Statement To Insert New Client Into Client Table

---
+++ 003-create-new-client.sql
@@ -0,0 +1,5 @@
+-- As a member of the Sales team I want to be able to create a new client. 
+ -- I should be able to store a name, address and phone number
+
+INSERT INTO `client` (`name`, address, phone) 
+	VALUES ('Taylor Corp', 'Buckingham Palace, SW1A 1AA', '07322343563');
