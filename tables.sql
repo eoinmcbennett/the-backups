69f6c3bf79de7c5a3e55763d207f99f4a112aa4b
chrisknos <christopher.paul@kainos.com>
2023-10-12 12:30:58 +0100

    Added client and technology tables#

--- tables.sql
+++ tables.sql
@@ -2,6 +2,15 @@ CREATE DATABASE thebackups_TaylorC;
 
 USE thebackups_TaylorC;
 
+CREATE TABLE client (
+    client_id SMALLINT UNSIGNED, 
+    name VARCHAR(255),
+    address VARCHAR(255), 
+    phone VARCHAR(11), 
+    employee_id SMALLINT UNSIGNED, 
+    win_date TIMESTAMP
+);
+
 CREATE TABLE project(
     `project_id` AUTO_INCREMENT PRIMARY KEY SMALLINT UNSIGNED,
     `name` VARCHAR(50) NOT NULL,
@@ -12,3 +21,9 @@ CREATE TABLE project(
     `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     CHECK(`value` > 0)
 );
+
+CREATE TABLE technology (
+technology_id SMALLINT UNSIGNED, 
+name VARCHAR(255)
+);
+
