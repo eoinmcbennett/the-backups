73a57b55b8fd32ca3a4e69f0032f975911f96102
Eoin <46245557+eoinmcbennett@users.noreply.github.com>
2023-10-12 15:59:08 +0100

    Update 003-sales-insert-client.sql

--- 003-sales-insert-client.sql
+++ 003-sales-insert-client.sql
@@ -1,1 +1,11 @@
+CREATE TABLE `client` (
+    `client_id` SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `address` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(11) NOT NULL,
+    `employee_id` SMALLINT UNSIGNED NULL,
+    `win_date` TIMESTAMP NULL,
+    FOREIGN KEY(employee_id) REFERENCES employee(employee_id)
+);
+
 INSERT INTO client(name,address,phone) VALUES('Test Client','Testing test street','0284173423');
